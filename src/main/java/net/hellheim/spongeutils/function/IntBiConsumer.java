9de84ae8d92a3a840a4d8c2a85741cc90fcde80d
MrHell228 <artemkz2016@gmail.com>
2024-11-26 03:01:58 +0300

    more Consumer<Vector> -> IntConsumer options

--- src/main/java/net/hellheim/spongeutils/function/IntBiConsumer.java
+++ src/main/java/net/hellheim/spongeutils/function/IntBiConsumer.java
@@ -3,7 +3,9 @@ package net.hellheim.spongeutils.function;
 import java.util.function.Consumer;
 
 import org.spongepowered.math.vector.Vector2d;
+import org.spongepowered.math.vector.Vector2f;
 import org.spongepowered.math.vector.Vector2i;
+import org.spongepowered.math.vector.Vector2l;
 
 @FunctionalInterface
 public interface IntBiConsumer {
@@ -12,6 +14,14 @@ public interface IntBiConsumer {
 	
 	static IntBiConsumer fromVector2i(final Consumer<Vector2i> consumer) {
 		return (x, y) -> consumer.accept(new Vector2i(x, y));
+	}
+	
+	static IntBiConsumer fromVector2l(final Consumer<Vector2l> consumer) {
+		return (x, y) -> consumer.accept(new Vector2l(x, y));
+	}
+	
+	static IntBiConsumer fromVector2f(final Consumer<Vector2f> consumer) {
+		return (x, y) -> consumer.accept(new Vector2f(x, y));
 	}
 	
 	static IntBiConsumer fromVector2d(final Consumer<Vector2d> consumer) {
--- src/main/java/net/hellheim/spongeutils/function/IntTriConsumer.java
+++ src/main/java/net/hellheim/spongeutils/function/IntTriConsumer.java
@@ -3,7 +3,9 @@ package net.hellheim.spongeutils.function;
 import java.util.function.Consumer;
 
 import org.spongepowered.math.vector.Vector3d;
+import org.spongepowered.math.vector.Vector3f;
 import org.spongepowered.math.vector.Vector3i;
+import org.spongepowered.math.vector.Vector3l;
 
 @FunctionalInterface
 public interface IntTriConsumer {
@@ -12,6 +14,14 @@ public interface IntTriConsumer {
 	
 	static IntTriConsumer fromVector3i(final Consumer<Vector3i> consumer) {
 		return (x, y, z) -> consumer.accept(new Vector3i(x, y, z));
+	}
+	
+	static IntTriConsumer fromVector3l(final Consumer<Vector3l> consumer) {
+		return (x, y, z) -> consumer.accept(new Vector3l(x, y, z));
+	}
+	
+	static IntTriConsumer fromVector3f(final Consumer<Vector3f> consumer) {
+		return (x, y, z) -> consumer.accept(new Vector3f(x, y, z));
 	}
 	
 	static IntTriConsumer fromVector3d(final Consumer<Vector3d> consumer) {
